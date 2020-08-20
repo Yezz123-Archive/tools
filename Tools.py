ec2d175328a632b2f4e571c7016dcb52d19330f2
Yasser Tahiri <yasserth19@gmail.com>
2020-08-20 17:05:01 +0100

    Edit 😔

--- Tools.py
+++ Tools.py
@@ -1,3 +1,9 @@
+#  Apache License
+#                           Version 2.0, January 2004
+#                        http://www.apache.org/licenses/
+#
+#  TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
 import os
 
 def menu():
